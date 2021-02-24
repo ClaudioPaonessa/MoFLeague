2a4334e0818578ad7dd4f9f3ef0d6c112368b26d
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-02-24 10:38:06 +0100

    update demo

--- cardTest.js
+++ cardTest.js
@@ -3,6 +3,6 @@ var app = angular.module("demoApp", []);
 app.controller("demoController", function($scope, $http) {
     $http.get("https://api.magicthegathering.io/v1/cards")
     .then(function(response) {
-        $scope.result = response.data;
+        $scope.result = response.data.cards;
     });
 });
