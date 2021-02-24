29245d34e29264c37d83f70fa7c50f510e421fc1
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-02-24 17:01:42 +0100

    update routes

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,13 @@ app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
 	        when('/', {
+                url='index.html',
 	            templateUrl: 'templates/register.html',
+	            controller: 'AccountController'
+	        })
+            .when('/home', {
+                url='app.html',
+	            templateUrl: 'templates/start.html',
 	            controller: 'AccountController'
 	        });
 }]);
