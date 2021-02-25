ccbda27462b3bba281d04dc6284022d4fcc57214
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-02-25 17:24:20 +0100

    tryining to fix controller

--- app/controllers/CardsController.js
+++ app/controllers/CardsController.js
@@ -1,16 +1,11 @@
-var URL = "https://mof-league.com"
-
 app.controller('CardsController', function($scope, $http){
  
     $scope.result = [];
     
-    $scope.init = function(){
-        $http({
-            url: URL + '/api/account/register.php',
-            method: 'POST',
-            data: $scope.form
-        }).then(function(data){
-            $scope.result.push(data.data);
+    $scope.init = function() {
+        $http.get("https://api.magicthegathering.io/v1/cards")
+        .then(function(response) {
+            $scope.result = response.data.cards;
         });
     }
 
--- app/controllers/TournamentsController.js
+++ app/controllers/TournamentsController.js
@@ -1,12 +1,11 @@
+var URL = "https://mof-league.com"
+
 app.controller("TournamentsController", function($scope, $http) {
     
     $scope.result = []
     
     $scope.init = function() {
-        $http.get("https://api.magicthegathering.io/v1/cards")
-        .then(function(response) {
-            $scope.result = response.data.cards;
-        });
+        
     }
 
     $scope.init();
