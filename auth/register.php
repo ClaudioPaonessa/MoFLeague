e851fff07f7887bb56002f94db1a8762e4f3257e
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-02-25 14:14:35 +0100

    update error for registration

--- auth/register.php
+++ auth/register.php
@@ -59,7 +59,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
         catch (PDOException $e)
         {
-            echo 'Query error.';
+            $username_err = "Username" . $username . "already taken";
+            $username = "";
             die();
         }
     }
