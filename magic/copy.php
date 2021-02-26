b9141e1095a32d8b3a0248fb2c5c4d5c90b5d503
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-02-26 17:34:28 +0100

    update card import

--- magic/copy.php
+++ magic/copy.php
@@ -39,10 +39,10 @@ foreach ($sets->data as &$set) {
     }
 }
 
-function loadCards($pdo, $url) {
+function loadCards($pdo, $set_id, $url) {
     //sleep(0.5);
 
-    $stmt = $pdo->prepare('INSERT INTO magic_cards (card_id_scryfall, card_name, card_image_uri, card_mana_cost) VALUES(:card_id_scryfall, :card_name, :card_image_uri, :card_mana_cost)');
+    $stmt = $pdo->prepare('INSERT INTO magic_cards (card_id_scryfall, magic_set_id, card_collector_number, card_name, card_type_line, card_image_uri, card_mana_cost, card_name_back, card_type_line_back, card_image_uri_back, card_mana_cost_back) VALUES(:card_id_scryfall, :magic_set_id, :card_collector_number, :card_name, :card_type_line, :card_image_uri, :card_mana_cost, :card_name_back, :card_type_line_back, :card_image_uri_back, :card_mana_cost_back)');
 
     $json_cards = file_get_contents($url);
     $cards = json_decode($json_cards);
@@ -55,14 +55,39 @@ function loadCards($pdo, $url) {
         try
         {
             $stmt->bindValue(':card_id_scryfall', $card->id);
-            $stmt->bindValue(':card_name',$card->name);
-            $stmt->bindValue(':card_image_uri',$card->released_at);
+            $stmt->bindValue(':card_collector_number', $card->collector_number);
 
-            if (isset($card->mana_cost)) {
-                $stmt->bindValue(':card_mana_cost', $card->mana_cost);
+            if (isset($card->card_faces)) {
+                $card_face_front = $card->card_faces[0];
+                $card_face_back = $card->card_faces[1];
+                
+                $stmt->bindValue(':magic_set_id', $set_id);
+                
+                $stmt->bindValue(':card_name', $card_face_front->name);
+                $stmt->bindValue(':card_type_line', $card_face_front->type_line);
+                $stmt->bindValue(':card_image_uri',$card_face_front->image_uris->normal);
+                $stmt->bindValue(':card_mana_cost', $card_face_front->mana_cost);
+                
+                $stmt->bindValue(':card_name_back', $card_face_back->name);
+                $stmt->bindValue(':card_type_line_back', $card_face_back->type_line);
+                $stmt->bindValue(':card_image_uri_back',$card_face_back->image_uris->normal);
+                $stmt->bindValue(':card_mana_cost_back', $card_face_back->mana_cost);
+
+                
             }
-            else{
-                $stmt->bindValue(':card_mana_cost', "");
+            else {
+                $card_face_front = $card;
+
+                $stmt->bindValue(':magic_set_id', $set_id);
+                $stmt->bindValue(':card_name', $card_face_front->name);
+                $stmt->bindValue(':card_type_line', $card_face_front->type_line);
+                $stmt->bindValue(':card_image_uri',$card_face_front->image_uris->normal);
+                $stmt->bindValue(':card_mana_cost', $card_face_front->mana_cost);
+
+                $stmt->bindValue(':card_name_back', "");
+                $stmt->bindValue(':card_type_line_back', "");
+                $stmt->bindValue(':card_image_uri_back',"");
+                $stmt->bindValue(':card_mana_cost_back', "");
             }
 
             $stmt->execute();
@@ -75,11 +100,11 @@ function loadCards($pdo, $url) {
 
     if (boolval($cards->has_more)) {
         if (isset($cards->next_page)) {
-            loadCards($pdo, $cards->next_page);
+            loadCards($pdo, $set_id, $cards->next_page);
         }
     }
 }
 
-loadCards($pdo, "https://api.scryfall.com/cards/search?order=set&q=e%3Akhm&unique=prints");
+loadCards($pdo, 9, "https://api.scryfall.com/cards/search?order=set&q=e%3Akhm&unique=prints");
 
 ?>
