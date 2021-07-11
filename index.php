1a2d67d5e510b2e0225a55c848cc8027ed3f21d0
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-07-11 11:38:57 +0200

    remove username from top right

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
             <!-- Navbar-->
             <ul class="navbar-nav ml-auto ml-md-0">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="userDropdown" data-target="#accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i> <?php echo $_SESSION["displayName"] ?></a>
+                    <a class="nav-link dropdown-toggle" id="userDropdown" data-target="#accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div id="accountDropdown" class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                         <a class="dropdown-item bg-danger text-white" href="/auth/logout">Logout</a>
                     </div>
