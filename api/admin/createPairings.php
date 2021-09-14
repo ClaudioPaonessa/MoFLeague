34410458f2a5ceaa0a72747d71347b29f6109a6d
Claudio Paonessa <claudio.paonessa@students.fhnw.ch>
2021-09-14 20:54:12 +0200

    group rounds in admin torunament view

--- api/admin/createPairings.php
+++ api/admin/createPairings.php
@@ -13,7 +13,6 @@ $roundId = getId();
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-var_dump($request);
 $pairingsString = $request->pairingsString;
 
 addPairings($roundId, $pairingsString);
